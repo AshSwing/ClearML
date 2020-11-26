0cf02b52e323c2d74b7e63cbe2c930e0f75e6829
AshSwing <han.zhiyan@outlook.com>
2020-11-26 15:36:33 +0800

    add K Nearest Neighbor Model

--- clearml/base/supervised_learning.py
+++ clearml/base/supervised_learning.py
@@ -1,4 +1,12 @@
 from .model import Model
 
 class SupervisedLearning(Model):
-    pass
+    
+    def fit(self, X_train, y_train, X_valid=None, y_valid=None):
+        pass
+
+    def predict(self, X, y_true):
+        pass
+
+    def _compare(self, y_true, y_pred, method='mse'):
+        pass
--- clearml/model/k_nearest_neighbor.py
+++ clearml/model/k_nearest_neighbor.py
@@ -1,4 +1,28 @@
+import re
+
+import numpy as np
+import pandas as pd
+
 from ..base import SupervisedLearning
 
 class KNearestNeighbor(SupervisedLearning):
-    pass
+    """K近邻模型
+    k: 近邻数量
+    distance_function: Ln or function
+    decision_function: voting, mean or function
+    """
+
+    def __init__(self, k, distance_function='L2', decision_function='voting'):
+        pass
+
+    def fit(self, X_train, y_train, X_valid=None, y_valid=None):
+        """模型训练
+        构造KD树
+        """
+        pass
+
+    def predict(self, X, y_true):
+        """模型预测
+        搜索KD树
+        """
+        pass
